683dfa720e8daadf126dfc41c55d559b35708bbe
graveyard233 <382287750@qq.com>
2022-09-08 16:34:18 +0800

    Initial commit

---
+++ app/src/main/java/com/lyd/tooltest/UI/Activity/MainActivity.java
@@ -0,0 +1,40 @@
+package com.lyd.tooltest.UI.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.flyco.tablayout.SlidingTabLayout;
+import com.lyd.tooltest.Base.BaseActivity;
+import com.lyd.tooltest.R;
+import com.lyd.tooltest.UI.Fragment.EventBusFragment;
+import com.lyd.tooltest.UI.Fragment.RetrofitFragment;
+
+import java.util.ArrayList;
+
+public class MainActivity extends BaseActivity {
+
+    private static final String TAG = "MainActivity";
+    private ArrayList<Fragment> fragments = new ArrayList<>();
+    private static final String[] titles = new String[] {"Retrofit","EventBus"};
+    private ViewPager viewPager;
+    private SlidingTabLayout tabLayout;
+
+    @Override
+    protected void initViews() {
+        viewPager = findViewById(R.id.main_viewPager);
+        tabLayout = findViewById(R.id.main_tabLayout);
+
+        fragments.add(new RetrofitFragment());
+        fragments.add(new EventBusFragment());
+
+        tabLayout.setViewPager(viewPager,titles,this,fragments);
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_main;
+    }
+}
---
+++ app/src/main/java/com/lyd/tooltest/UI/Fragment/RetrofitFragment.java
@@ -0,0 +1,16 @@
+package com.lyd.tooltest.UI.Fragment;
+
+import com.lyd.tooltest.Base.BaseFragment;
+import com.lyd.tooltest.R;
+
+public class RetrofitFragment extends BaseFragment {
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_retrofit;
+    }
+}
