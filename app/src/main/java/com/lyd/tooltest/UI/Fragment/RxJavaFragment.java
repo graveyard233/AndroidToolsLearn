5aa156d94fc8a45a3546ac455da7ad16648edeaf
graveyard233 <382287750@qq.com>
2022-09-14 17:27:52 +0800

    准备应用AgentWeb来实现webView

--- app/src/main/java/com/lyd/tooltest/UI/Fragment/RxJavaFragment.java
+++ app/src/main/java/com/lyd/tooltest/UI/Fragment/RxJavaFragment.java
@@ -1,9 +1,13 @@
 package com.lyd.tooltest.UI.Fragment;
 
+import com.just.agentweb.AgentWebView;
 import com.lyd.tooltest.Base.BaseFragment;
 import com.lyd.tooltest.R;
 
 public class RxJavaFragment extends BaseFragment {
+
+    private AgentWebView webView;
+
     @Override
     protected void initViews() {
 
