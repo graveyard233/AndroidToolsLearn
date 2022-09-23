2660169f0b4bfc2a31330902afa272b6485b6431
graveyard233 <382287750@qq.com>
2022-09-23 17:18:01 +0800

    尝试获取评论数据 准备尝试拦截post请求

--- app/src/main/java/com/lyd/tooltest/UI/Fragment/RxJavaFragment.java
+++ app/src/main/java/com/lyd/tooltest/UI/Fragment/RxJavaFragment.java
@@ -1,13 +1,13 @@
 package com.lyd.tooltest.UI.Fragment;
 
-import android.graphics.Bitmap;
 import android.util.Log;
 import android.view.ViewGroup;
 import android.webkit.ValueCallback;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
 import android.webkit.WebView;
 import android.widget.LinearLayout;
 
-import com.blankj.utilcode.util.EncodeUtils;
 import com.just.agentweb.AgentWeb;
 import com.just.agentweb.WebViewClient;
 import com.lyd.tooltest.Base.BaseFragment;
@@ -30,7 +30,8 @@ public class RxJavaFragment extends BaseFragment {
     @Override
     protected void initViews() {
         linear = find(R.id.rxj_linearLayout);
-        setWebView("https://iyingdi.com/tz/post/5218038");
+
+        setWebView("https://iyingdi.com/tz/post/5218587");
     }
 
     private WebViewClient webViewClient = new WebViewClient(){
@@ -54,32 +55,52 @@ public class RxJavaFragment extends BaseFragment {
         }
 
         @Override
+        public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
+            if (request.getMethod().equals("POST")){
+                Log.i("TAG", "shouldInterceptRequest: " + request.getUrl().toString());
+            }
+
+            return super.shouldInterceptRequest(view, request);
+        }
+
+        @Override
         public void onPageFinished(WebView view, String url) {
 
 
             super.onPageFinished(view, url);
-//            view.loadUrl(MyJs.getHtml);//加载js
+            view.loadUrl(MyJs.getHtml);//加载js
 
 
-//            webView.getJsAccessEntrace().quickCallJs("getHtmlCode", new ValueCallback<String>() {
-//                @Override
-//                public void onReceiveValue(String value) {
-//                    flag_html++;
-//                    if (flag_html % 2 == 0){
-//
-//                        String trueHtml = StringEscapeUtils.unescapeEcmaScript(value);
-////                    Log.d("TAG", "onReceiveValue: " + trueHtml);//先做解码
-//
-//
-//
-//                        Document doc = Jsoup.parse(trueHtml);
-//
+            webView.getJsAccessEntrace().quickCallJs("getHtmlCode", new ValueCallback<String>() {
+                @Override
+                public void onReceiveValue(String value) {
+                    flag_html++;
+                    if (flag_html % 2 == 0){
+
+                        String trueHtml = StringEscapeUtils.unescapeEcmaScript(value);
+//                    Log.d("TAG", "onReceiveValue: " + trueHtml);//先做解码
+
+
+                        //获取整个html的代码
+                        Document doc = Jsoup.parse(trueHtml);
+
+                        //这个是去找悬浮窗，但是已经被我干掉了，所以这两行是废的
 //                        Elements body = doc.select("div.down-load-app-container-post");
 //                        Log.d("TAG", "body: " + body.toString());
-//                    }
-//
-//                }
-//            });
+
+                        //获取评论的div，是最外面的一层，能显示的评论数据都在这里
+                        //有个问题，就是有可能网络不好或评论数据过大，网页加载好的时候评论div还是空的
+                        Element comments = doc.getElementById("viewComments");
+                        Elements boxes = comments.select("div.comments-box");
+                        for (int i = 0; i < boxes.size(); i++) {
+                            Log.i("TAG", "boxes size: " + boxes.get(i).toString());
+                        }
+
+//                        Log.i("TAG", "viewComments: " + comments.toString());
+                    }
+
+                }
+            });
 
 
         }
