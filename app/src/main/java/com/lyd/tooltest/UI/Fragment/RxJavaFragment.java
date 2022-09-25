374bdda2d9e15287ef60bc9315130baaa6dc4fb3
graveyard233 <382287750@qq.com>
2022-09-25 23:29:57 +0800

    知道了在什么时候获取评论代码 但因为有概率获取错误，思路有点混乱，还得再想想

--- app/src/main/java/com/lyd/tooltest/UI/Fragment/RxJavaFragment.java
+++ app/src/main/java/com/lyd/tooltest/UI/Fragment/RxJavaFragment.java
@@ -20,6 +20,16 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.ObservableEmitter;
+import io.reactivex.rxjava3.core.ObservableOnSubscribe;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.functions.Function;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
 public class RxJavaFragment extends BaseFragment {
 
     private AgentWeb webView;
@@ -31,7 +41,42 @@ public class RxJavaFragment extends BaseFragment {
     protected void initViews() {
         linear = find(R.id.rxj_linearLayout);
 
-        setWebView("https://iyingdi.com/tz/post/5218587");
+//        setWebView("https://iyingdi.com/tz/post/5218768");
+
+        Observable.just("132")
+                .map(new Function<String, Boolean>() {
+                    @Override
+                    public Boolean apply(String s) throws Throwable {
+                        if (s.equals("132"))
+                            return true;
+                        else return false;
+                    }
+                })
+
+                .subscribe(new Observer<Boolean>() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(@NonNull Boolean aBoolean) {
+                        if (aBoolean)
+                            Log.i("TAG", "onNext: yes" );
+                        else
+                            Log.e("TAG", "onNext: error");
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
     }
 
     private WebViewClient webViewClient = new WebViewClient(){
@@ -40,25 +85,50 @@ public class RxJavaFragment extends BaseFragment {
         public void onLoadResource(WebView view, String url) {
 
             super.onLoadResource(view, url);
-
-            flag_html++;
-            if (flag_html %2 == 1){
-
-                view.evaluateJavascript("javascript: document.getElementsByClassName('down-load-app-container-post')[0].style.display = 'none';",
+            view.evaluateJavascript("javascript: document.getElementsByClassName('down-load-app-container-post')[0].style.display = 'none';",
                         new ValueCallback<String>() {
                             @Override
                             public void onReceiveValue(String value) {
                                 Log.e("TAG", "finish: "+ value);
                             }
                         });
-            }
+
         }
 
         @Override
         public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
             if (request.getMethod().equals("POST")){
-                Log.i("TAG", "shouldInterceptRequest: " + request.getUrl().toString());
+                // TODO: 2022/9/25 这里获取时，有小概率会拿不到数据，需要反复异步请求
+                if (request.getUrl().toString().startsWith("https://www.google")){
+                    Log.i("TAG", "shouldInterceptRequest: " + request.getUrl().toString());
+                    webView.getJsAccessEntrace().quickCallJs("getHtmlCode", new ValueCallback<String>() {
+                        @Override
+                        public void onReceiveValue(String value) {
+                            if (value != null && !value.equals("null")){
+                                String trueHtml = StringEscapeUtils.unescapeEcmaScript(value);
+                                Document doc = Jsoup.parse(trueHtml);
+
+                                try {
+                                    Element comments = doc.getElementById("viewComments");
+//                                    Log.i("TAG", "comments: " + comments.toString());
+                                    Elements boxes = comments.select("div.comments-box");
+                                    Log.i("TAG", "boxes size: " + boxes.size());
+//                                    for (int i = 0; i < boxes.size(); i++) {
+//                                        Log.i("TAG", "boxes size: " + boxes.get(i).toString());
+//                                    }
+                                } catch (Exception e){
+                                    e.printStackTrace();
+                                }
+                            }
+
+
+                        }
+                    });
+
+                }
+
             }
+
 
             return super.shouldInterceptRequest(view, request);
         }
@@ -68,40 +138,14 @@ public class RxJavaFragment extends BaseFragment {
 
 
             super.onPageFinished(view, url);
-            view.loadUrl(MyJs.getHtml);//加载js
-
-
-            webView.getJsAccessEntrace().quickCallJs("getHtmlCode", new ValueCallback<String>() {
+//            view.loadUrl(MyJs.getHtml);//加载js
+            //注入js，但没调用
+            view.evaluateJavascript(MyJs.getHtml, new ValueCallback<String>() {
                 @Override
                 public void onReceiveValue(String value) {
-                    flag_html++;
-                    if (flag_html % 2 == 0){
-
-                        String trueHtml = StringEscapeUtils.unescapeEcmaScript(value);
-//                    Log.d("TAG", "onReceiveValue: " + trueHtml);//先做解码
-
-
-                        //获取整个html的代码
-                        Document doc = Jsoup.parse(trueHtml);
-
-                        //这个是去找悬浮窗，但是已经被我干掉了，所以这两行是废的
-//                        Elements body = doc.select("div.down-load-app-container-post");
-//                        Log.d("TAG", "body: " + body.toString());
-
-                        //获取评论的div，是最外面的一层，能显示的评论数据都在这里
-                        //有个问题，就是有可能网络不好或评论数据过大，网页加载好的时候评论div还是空的
-                        Element comments = doc.getElementById("viewComments");
-                        Elements boxes = comments.select("div.comments-box");
-                        for (int i = 0; i < boxes.size(); i++) {
-                            Log.i("TAG", "boxes size: " + boxes.get(i).toString());
-                        }
-
-//                        Log.i("TAG", "viewComments: " + comments.toString());
-                    }
 
                 }
             });
-
 
         }
     };
