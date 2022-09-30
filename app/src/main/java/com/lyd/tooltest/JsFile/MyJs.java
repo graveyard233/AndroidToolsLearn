445e954d5b5ee44931bb9b01d1471cecad89a378
graveyard233 <382287750@qq.com>
2022-09-30 12:38:33 +0800

    准备对获取评论时做分类讨论 有可能有热评和没热评

--- app/src/main/java/com/lyd/tooltest/JsFile/MyJs.java
+++ app/src/main/java/com/lyd/tooltest/JsFile/MyJs.java
@@ -6,11 +6,12 @@ public class MyJs {
             "return '<html>' + content + '</html>'" +
             "}";
 
-    public static final String removeFloatBar = "javascript: document.getElementsByClassName('down-load-app-container-post')[0].innerHTML = " +
-            "'" +
-//            "<font style=\"float:left;margin-left:5px;font-size:.42rem;margin-top:3px;\">旅法师营地</font>" +
-            "<p>旅法师营地</p>" +
-            "';";
+    public static final String removeFloatBar = "javascript: document.getElementsByClassName('down-load-app-container-post')[0].style.display = 'none';";
+//            "javascript: document.getElementsByClassName('down-load-app-container-post')[0].innerHTML = " +
+//            "'" +
+////            "<font style=\"float:left;margin-left:5px;font-size:.42rem;margin-top:3px;\">旅法师营地</font>" +
+//            "<p>旅法师营地</p>" +
+//            "';";
 
 
 }
--- app/src/main/java/com/lyd/tooltest/UI/Fragment/RxJavaFragment.java
+++ app/src/main/java/com/lyd/tooltest/UI/Fragment/RxJavaFragment.java
@@ -1,5 +1,8 @@
 package com.lyd.tooltest.UI.Fragment;
 
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.util.Log;
 import android.view.ViewGroup;
 import android.webkit.ValueCallback;
@@ -7,6 +10,8 @@ import android.webkit.WebResourceRequest;
 import android.webkit.WebResourceResponse;
 import android.webkit.WebView;
 import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
 
 import com.just.agentweb.AgentWeb;
 import com.just.agentweb.WebViewClient;
@@ -21,7 +26,6 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
-import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.ObservableEmitter;
 import io.reactivex.rxjava3.core.ObservableOnSubscribe;
@@ -32,6 +36,8 @@ import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class RxJavaFragment extends BaseFragment {
 
+    private static final int GET_COMMENT_DELAY = 100;
+
     private AgentWeb webView;
     private LinearLayout linear;
 
@@ -41,42 +47,8 @@ public class RxJavaFragment extends BaseFragment {
     protected void initViews() {
         linear = find(R.id.rxj_linearLayout);
 
-//        setWebView("https://iyingdi.com/tz/post/5218768");
+        setWebView("https://iyingdi.com/tz/post/5219116");//5219071
 
-        Observable.just("132")
-                .map(new Function<String, Boolean>() {
-                    @Override
-                    public Boolean apply(String s) throws Throwable {
-                        if (s.equals("132"))
-                            return true;
-                        else return false;
-                    }
-                })
-
-                .subscribe(new Observer<Boolean>() {
-                    @Override
-                    public void onSubscribe(@NonNull Disposable d) {
-
-                    }
-
-                    @Override
-                    public void onNext(@NonNull Boolean aBoolean) {
-                        if (aBoolean)
-                            Log.i("TAG", "onNext: yes" );
-                        else
-                            Log.e("TAG", "onNext: error");
-                    }
-
-                    @Override
-                    public void onError(@NonNull Throwable e) {
-
-                    }
-
-                    @Override
-                    public void onComplete() {
-
-                    }
-                });
     }
 
     private WebViewClient webViewClient = new WebViewClient(){
@@ -85,7 +57,7 @@ public class RxJavaFragment extends BaseFragment {
         public void onLoadResource(WebView view, String url) {
 
             super.onLoadResource(view, url);
-            view.evaluateJavascript("javascript: document.getElementsByClassName('down-load-app-container-post')[0].style.display = 'none';",
+            view.evaluateJavascript(MyJs.removeFloatBar,
                         new ValueCallback<String>() {
                             @Override
                             public void onReceiveValue(String value) {
@@ -101,29 +73,9 @@ public class RxJavaFragment extends BaseFragment {
                 // TODO: 2022/9/25 这里获取时，有小概率会拿不到数据，需要反复异步请求
                 if (request.getUrl().toString().startsWith("https://www.google")){
                     Log.i("TAG", "shouldInterceptRequest: " + request.getUrl().toString());
-                    webView.getJsAccessEntrace().quickCallJs("getHtmlCode", new ValueCallback<String>() {
-                        @Override
-                        public void onReceiveValue(String value) {
-                            if (value != null && !value.equals("null")){
-                                String trueHtml = StringEscapeUtils.unescapeEcmaScript(value);
-                                Document doc = Jsoup.parse(trueHtml);
-
-                                try {
-                                    Element comments = doc.getElementById("viewComments");
-//                                    Log.i("TAG", "comments: " + comments.toString());
-                                    Elements boxes = comments.select("div.comments-box");
-                                    Log.i("TAG", "boxes size: " + boxes.size());
-//                                    for (int i = 0; i < boxes.size(); i++) {
-//                                        Log.i("TAG", "boxes size: " + boxes.get(i).toString());
-//                                    }
-                                } catch (Exception e){
-                                    e.printStackTrace();
-                                }
-                            }
-
-
-                        }
-                    });
+                    Message message = new Message();
+                    message.what = GET_COMMENT_DELAY;
+//                    handler.sendMessage(message);
 
                 }
 
@@ -147,6 +99,10 @@ public class RxJavaFragment extends BaseFragment {
                 }
             });
 
+
+            Message message = new Message();
+            message.what = GET_COMMENT_DELAY;
+            handler.sendMessage(message);
         }
     };
 
@@ -159,6 +115,58 @@ public class RxJavaFragment extends BaseFragment {
                 .ready()
                 .go(url);
     }
+
+    private void getComments(AgentWeb webView){
+        webView.getJsAccessEntrace().quickCallJs("getHtmlCode", new ValueCallback<String>() {
+            @Override
+            public void onReceiveValue(String value) {
+                if (value != null && !value.equals("null")){
+                    String trueHtml = StringEscapeUtils.unescapeEcmaScript(value);
+                    Document doc = Jsoup.parse(trueHtml);
+
+                    try {
+                        Element comments = doc.getElementById("viewComments");
+//                                    Log.i("TAG", "comments: " + comments.toString());
+                        Elements boxes = comments.select("div.comments-box");
+//                                    Log.i("TAG", "boxes size: " + boxes.size());
+                        if (boxes.size() == 1){// 等于1的话就有可能没评论或者现在网页端还没异步加载出来
+                            if (boxes.get(0).select("span.select").get(0).text().equals("全部评论 0条")){
+                                Log.e("TAG", "这个帖子没有评论");
+                            } else {
+                                //有评论，分为两个情况来讨论，
+                                // 一是没有热评，只有普通评论，也只有一个box，所以要看评论量，也可以直接找
+                                // 二是但还没加载出来，所以要再等一下再获取
+                                Log.e("TAG", "有评论，但还没加载出来，等一下再拿 ");
+                                Log.i("TAG", boxes.get(0).toString());
+                                Message message = new Message();
+                                message.what = GET_COMMENT_DELAY;
+//                                handler.sendMessageDelayed(message,1000);
+                            }
+                        } else { //box 的size 不等于1，即这个帖子有评论了，可以拿其中的数据
+                            Log.d("TAG", "数据来了");
+                            for (int i = 0; i < boxes.size(); i++) {
+                                Log.i("TAG", "boxes size: " + boxes.get(i).toString());
+                            }
+                        }
+
+                    } catch (Exception e){
+                        e.printStackTrace();
+                    }
+                }
+
+
+            }
+        });
+    }
+
+    private Handler handler = new Handler(Looper.getMainLooper()){
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            if (msg.what == GET_COMMENT_DELAY){
+                getComments(webView);
+            }
+        }
+    };
 
     @Override
     protected int getLayoutId() {
